5d67e7768584a4fa50bdb23bd07aaf901cbb9c3b
ClementG60 <cle_guillaume@hotmail.fr>
2021-11-03 10:52:05 +0100

    modification code

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,10 @@
 let days = ['Lundi', 'Mardi', 'Mercredi', 'Jeudi', 'Vendredi', 'Samedi','Dimanche'];
+for (let i = 0; i < days.length; i++){
+    if ((days[i] == "Samedi") || (days[i] == "Dimanche")){
+        document.write(days[i].bold(), "<br>")
+
+    } else {
+        document.write(days[i], "<br>")
+    }
+}
+
